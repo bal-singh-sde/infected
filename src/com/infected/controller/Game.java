96551ad33f3dc81f5afc57565a5d98f8c7988746
Rasoj Timsina <rasoj_timsina@yahoo.com>
2021-06-03 16:21:55 -0500

    AB#222 Introduction part of the game

--- src/com/infected/controller/Game.java
+++ src/com/infected/controller/Game.java
@@ -1,7 +1,25 @@
 package com.infected.controller;
 
+import java.util.Scanner;
+
 public class Game {
     public void start() {
-        System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("# Welcome to the World of INFECTED!!!!!!!");
+        //System.out.println("        \n>> Enter '1' to start the game.\n        >> Enter '0' to quit the game.");
+        System.out.println("\nA new virus have spread around the community and there are chances that you can get infected. \n" +
+                "\nYou are at home you have the option to quarantine(lower contamination level to 0)or go west. \nSelect the following command:");
+        System.out.println("\n1: goWest \n2: quarantine \n3: q to Quit ");
+        System.out.print("Enter your command here: ");
+        String input = scan.nextLine();
+         if(input.equals("q")) {
+            System.exit(0);
+         }
+         else if(input.equals("goWest")){
+            System.out.println("You are on Main Block Two. You have the option to go east, west, or north");
+        }{
+             System.out.println("You are quarantined. Your contamination level is 0. You have the option to go west or quit");
+         }
     }
 }
