b75a28b95ce0fb8702d9e564173c35df56931d24
raymond-sde <raymond.aranda.sde@gmail.com>
2021-06-07 12:52:01 -0700

    AB#245, read and write from Player.json

--- src/com/infected/model/Player.java
+++ src/com/infected/model/Player.java
@@ -1,13 +1,35 @@
 package com.infected.model;
 
-public class Player {
-    private static String currentLocation = "home";
+import com.fasterxml.jackson.databind.JsonNode;
+import com.infected.util.TextParser;
 
-    public static String getCurrentLocation() {
-        return currentLocation;
+import java.io.File;
+import java.io.IOException;
+
+public class Player {
+    private static final File jsonSource = new File("./data/Player.json");
+
+    public static JsonNode getPlayerNode() {
+        try {
+            return TextParser.parse(jsonSource);
+        } catch(IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return null;
     }
 
-    public static void setCurrentLocation(String currentLocation) {
-        Player.currentLocation = currentLocation;
+    public static String getCurrentLocation() {
+        return getPlayerNode().get("currentLocation").textValue();
+    }
+
+    public static void setCurrentLocation(String newLocation) {
+        JsonNode currPlayerNode = getPlayerNode();
+        JsonNode newNode = TextParser.getNewNode(currPlayerNode, "currentLocation", newLocation);
+
+        try {
+            TextParser.write(jsonSource, newNode);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
     }
 }
