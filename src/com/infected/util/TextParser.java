2f568dcf5523f7936a9dc48782239eba17809f4e
raymond-sde <raymond.aranda.sde@gmail.com>
2021-06-07 10:05:27 -0700

    AB#245, added write() to TextParser, refactored getWorldStats()

--- src/com/infected/util/TextParser.java
+++ src/com/infected/util/TextParser.java
@@ -1,7 +1,10 @@
 package com.infected.util;
 
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,11 +14,22 @@ public class TextParser {
     private static ObjectMapper objectMapper = getDefaultObjectMapper(); //only need one object mapper
 
     public static ObjectMapper getDefaultObjectMapper() {
-        ObjectMapper defaultObjectMapper = new ObjectMapper();
-        return defaultObjectMapper;
+        return new ObjectMapper();
     }
 
     public static JsonNode parse(File src) throws IOException {
         return objectMapper.readTree(src);
+    }
+
+    public static JsonNode getNewNode(JsonNode jsonNode, String key, String newValue) {
+        ((ObjectNode) jsonNode).put(key, newValue);
+        return jsonNode;
+    }
+
+    public static void write(File src, JsonNode jsonNode) throws IOException {
+        // use DefaultPrettyPrinter to auto-format json after writing
+        ObjectWriter writer = objectMapper.writer(new DefaultPrettyPrinter());
+        // replace current json with new JsonNode
+        writer.writeValue(src, jsonNode);
     }
 }
