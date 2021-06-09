ff1920121fa59c0c9113104528c3bc3e6dacb284
raymond-sde <raymond.aranda.sde@gmail.com>
2021-06-09 11:14:09 -0700

    AB#239, create Game model class, add getNewNode helper in TextParser

---
+++ src/com/infected/model/Game.java
@@ -0,0 +1,54 @@
+package com.infected.model;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.infected.util.TextParser;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Game {
+    private static final File jsonSource = new File("./data/Game.json");
+
+    private static JsonNode getGameNode() {
+        try {
+            return TextParser.parse(jsonSource);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return null;
+    }
+
+    public static boolean isGameSaved() {
+        try {
+            return TextParser.parse(jsonSource).get("isGameSaved").asBoolean();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void saveGame() {
+        if (!isGameSaved()) {
+            JsonNode newGameNode = TextParser.getNewNode(getGameNode(), "isGameSaved", true);
+            try {
+                TextParser.write(jsonSource, newGameNode);
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    public static void clearGameData() {
+        Player.resetPlayer();
+        resetGame();
+    }
+
+    private static void resetGame() {
+        JsonNode newGameNode = TextParser.getNewNode(getGameNode(), "isGameSaved", false);
+        try {
+            TextParser.write(jsonSource, newGameNode);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+}
