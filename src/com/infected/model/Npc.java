cf35a963ad68648e52225aa9846934f0a888c855
PatWil <patrick.williams@tlgcohort.com>
2021-06-11 06:48:10 -0700

    found un-needed comments,AB#53

--- src/com/infected/model/Npc.java
+++ src/com/infected/model/Npc.java
@@ -20,17 +20,5 @@ import java.io.IOException;
     public abstract String getGreeting();
     public abstract String getDialogue();
     public abstract int getRisk();
-//    public static void checkForNpc(){
-//    if (getNpcNode().has(Player.getCurrentLocation())){
-//        int npcLevel = getRisk(Player.getCurrentLocation());
-//        Player.raiseContaminationLevel(npcLevel);
-//        System.out.println(getGreeting(Player.getCurrentLocation())+", "+getDialogue(Player.getCurrentLocation()));
-//    }
-//    }
-//    public static void main(String[]args){
-//        System.out.println(getNpcNode());
-//        System.out.println(getGreeting("park"));
-//        System.out.println(getDialogue("park"));
-//        System.out.println(getRisk("park"));
-//    }
+
 }
