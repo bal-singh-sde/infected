3aa6c80955c81b559a5c90713e8ebc595204481b
raymond-sde <raymond.aranda.sde@gmail.com>
2021-06-14 17:56:20 -0700

    AB#367, Update map with new position during navigation

--- src/com/infected/util/AsciiParser.java
+++ src/com/infected/util/AsciiParser.java
@@ -6,14 +6,32 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AsciiParser {
-    public static void parse(Path path) throws IOException {
-        Files.lines(path).forEach(System.out::println);
+    public static List<String> parse(Path path) throws IOException {
+        List<String> asciiLines = new ArrayList<>();
+        Files.lines(path).forEach(asciiLines::add);
+        return asciiLines;
     }
 
-    public static void write(File src, String output) throws IOException {
-        PrintWriter out = new PrintWriter(new FileWriter(src));
-        out.println(output);
+    public static void write(File src, List<String> asciiLines) throws IOException {
+        // clear existing contents of file
+        AsciiParser.clearFile(src);
+        PrintWriter pw = new PrintWriter(new FileWriter(src, true));
+        // append each line to empty file
+        for (String line : asciiLines) {
+            pw.println(line);
+        }
+        pw.close();
+    }
+
+    public static void clearFile(File src) throws IOException {
+        FileWriter fw = new FileWriter(src, false);
+        PrintWriter pw = new PrintWriter(fw, false);
+        pw.flush();
+        pw.close();
+        fw.close();
     }
 }
